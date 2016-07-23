1588a0a33b7f7203d97bc4e011279b6617eb25c3
krishnbharathi <krishnbharathi@gmail.com>
2016-07-23 18:40:19 +0530

    update

---
+++ newproject/src/newproject/NewPush.java
@@ -0,0 +1,16 @@
+package newproject;
+
+public class NewPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+System.out.println("done");
+System.out.println("done");
+System.out.println("done");
+
+
+	}
+
+}
